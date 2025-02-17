52239b14a30dda75428d441580aee5d9402c4b31
Vikas Poddar <vikas.poddar@walmart.com>
2025-02-17 16:49:52 +0530

    Add lib code

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub fn public_function() {
+    println!("called `public_function()`");
+}
+
+fn private_function() {
+    println!("called `private_function()`");
+}
+
+pub fn indirect_access() {
+    println!("called  `indirect_access()`, and then call private `private_function()`");
+    private_function();
+}
